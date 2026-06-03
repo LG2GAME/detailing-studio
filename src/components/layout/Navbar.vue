153d47ff01d939fc92bb3023ccac9b39dc78e096
LG2GAME <lg2game6@gmail.com>
2026-06-03 17:24:05 +0200

    feat(navbar): add component

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,119 @@
+<template>
+  <nav class="navbar">
+    <div class="logo">Pawel Wnek</div>
+    <div class="burger" @click="toggleMenu" role="button" tabindex="0">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    <ul :class="{active: isOpen}">
+      <li @click="isOpen = false"><a href="#services">Services</a></li>
+      <li @click="isOpen = false"><a href="#gallery">Gallery</a></li>
+      <li @click="isOpen = false"><a href="#about-us">About us</a></li>
+      <li @click="isOpen = false"><a href="#book-now">Book now</a></li>
+      <li @click="isOpen = false"><a href="#contact">Contact</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+import {onBeforeUnmount, ref, watch} from "vue";
+
+const isOpen = ref(false);
+
+const toggleMenu = () => {
+  isOpen.value = !isOpen.value;
+}
+
+watch(isOpen, (value) => {
+  document.body.style.overflow = value ? "hidden" : "";
+})
+
+onBeforeUnmount(() => {
+  document.body.style.overflow = ""
+})
+</script>
+
+<style lang="scss" scoped>
+  .navbar {
+    align-items: center;
+    display: flex;
+    font-family: var(--ff-lato), sans-serif;
+    justify-content: space-between;
+    padding: 25px 40px;
+    width: 100%;
+    z-index: 99;
+
+
+    .logo {
+      font-size: var(--fs-medium);
+      font-weight: 700;
+      text-transform: uppercase;
+    }
+
+    .burger {
+      cursor: pointer;
+      display: none;
+      flex-direction: column;
+      gap: 8px;
+      width: 45px;
+
+      span {
+        background-color: var(--cl-white);
+        border-radius: 99px;
+        height: 5px;
+        width: 100%;
+      }
+    }
+
+    ul {
+      align-items: center;
+      display: flex;
+      gap: 25px;
+      list-style-type: none;
+
+      li {
+        font-size: var(--fs-small);
+        text-transform: uppercase;
+        cursor: pointer;
+      }
+    }
+  }
+
+  @media (max-width: 992px) {
+    .navbar {
+      position: relative;
+
+      .burger {
+        display: flex;
+      }
+
+      .burger, .logo {
+        z-index: 99;
+      }
+
+      ul {
+        align-items: center;
+        background-color: var(--cl-black);
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        left: 0;
+        position: absolute;
+        top: 0;
+        transform: translateX(-105%);
+        transition: transform .3s ease-in-out;
+        width: 100%;
+
+        &.active {
+          transform: translateX(0);
+        }
+
+        li {
+          font-size: var(--fs-medium);
+          font-weight: 700;
+        }
+      }
+    }
+  }
+</style>
