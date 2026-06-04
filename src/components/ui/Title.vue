838d85d657c5506927288912c37d8441fb612af0
LG2GAME <lg2game6@gmail.com>
2026-06-04 15:50:51 +0200

    feat(title): add component

---
+++ src/components/ui/Title.vue
@@ -0,0 +1,22 @@
+<template>
+  <component :is="tag" :class="{title: tag === 'h1'}">
+    <slot />
+  </component>
+</template>
+
+<script setup>
+  defineProps({
+    tag: {
+      type: String,
+      default: 'h1'
+    }
+  })
+</script>
+
+<style lang="scss" scoped>
+  .title {
+    border-bottom: 4px solid var(--cl-graphite);
+    padding: 10px;
+    display: inline-block;
+  }
+</style>
