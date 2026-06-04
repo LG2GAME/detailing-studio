d0323f29c420a3099c9ca71a22b8567896d4a97d
LG2GAME <lg2game6@gmail.com>
2026-06-04 15:50:46 +0200

    feat(card): add component

---
+++ src/components/ui/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+  .card {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    gap: 25px;
+    height: 100%;
+  }
+</style>
