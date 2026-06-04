8fc7c5ffdf1b9d97391df20f1cd0244a44ed25f5
LG2GAME <lg2game6@gmail.com>
2026-06-04 15:59:15 +0200

    feat(services): add section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,12 @@
   <Navbar />
   <main>
     <Hero />
+    <Services />
   </main>
 </template>
 
 <script setup>
   import Navbar from "@/components/layout/Navbar.vue";
   import Hero from "@/components/sections/Hero.vue";
+  import Services from "@/components/sections/Services.vue";
 </script>
