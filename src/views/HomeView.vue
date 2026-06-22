75905e86297d76fce600e595c7c67879008302b9
LG2GAME <lg2game6@gmail.com>
2026-06-22 21:36:08 +0200

    perf(gallery): gallery lazy loading

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,11 +11,16 @@
 </template>
 
 <script setup>
+import { defineAsyncComponent } from 'vue'
+
 import AppNavbar from '@/components/layout/AppNavbar.vue'
 import HeroSection from '@/components/sections/HeroSection.vue'
 import ServicesSection from '@/components/sections/ServicesSection.vue'
-import GallerySection from '@/components/sections/GallerySection.vue'
 import AboutUsSection from '@/components/sections/AboutUsSection.vue'
 import BookNowSection from '@/components/sections/BookNowSection.vue'
 import AppFooter from '@/components/layout/AppFooter.vue'
+
+const GallerySection = defineAsyncComponent(
+  () => import('@/components/sections/GallerySection.vue')
+)
 </script>
