93d528a70139ba7e61bc8b216adabc8733f0e5b8
LG2GAME <lg2game6@gmail.com>
2026-06-28 16:20:27 +0200

    Revert "chore(accessibility): improvement nav acc"
    This reverts commit 5160b8bb1def3645f2135e915c68a4fe9fd16a45.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import VueCookies from 'vue-cookies'
 import { createApp } from 'vue'
 import { createHead } from '@unhead/vue/client'
 
@@ -10,12 +9,5 @@ const app = createApp(App)
 const head = createHead()
 
 app.use(head)
-app.use(VueCookies, {
-  expire: '365d',
-  path: '/',
-  domain: '',
-  secure: true,
-  sameSite: 'Lax'
-})
 
 app.mount('#app')
